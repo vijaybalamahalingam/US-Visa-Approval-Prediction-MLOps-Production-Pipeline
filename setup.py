eaf5f418205a36caaf3ac3ef8aef0c0a9f73f697
Vijay <vijay_5@Vijays-MacBook-Air.local>
2025-01-18 22:49:18 +0530

    feat: add requirements and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="vijaybalamahalingam",
+    author_email="vijaybala.mahalingam@gmail.com",
+    packages=find_packages()
+)
