e79535d5e284a0f8d9fbbc50e2cff39eba1d1309
Vijay <vijay_5@Vijays-MacBook-Air.local>
2025-01-22 22:05:14 +0530

    feat: add data validation

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -17,6 +17,11 @@ TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -25,3 +30,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
